a91892463d022cc42f895804ed9b583ce3193227
Moaaz <moaaz.elsharkawy2017@gmail.com>
2023-05-22 15:21:18 +0300

    components

---
+++ src/comp/footer.js
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+        <footer>
+        Designed by pioneer
+        <span>🧡</span>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
--- src/pages/service.js
+++ src/pages/service.js
@@ -1,131 +1,13 @@
 import React from "react";
 import { FaSignInAlt, FaHome, FaPlus } from "react-icons/fa";
+import Header from "../comp/header";
+import Footer from '../comp/footer';
+
+
 const Service = () => {
   return (
     <>
-      <header className="hide-when-mobile">
-        <h1>piadvservice</h1>
-        <ul className="flex">
-          <li className="main-list">
-            <a className="main-link" href="#">
-              Advertise with us
-            </a>
-            <ul className="sub-ul">
-              <li>
-                <a href="">product</a>
-              </li>
-              <li>
-                <a href="">company</a>
-              </li>
-            </ul>
-          </li>
-          <li className="main-list">
-            <a className="main-link" href="#">
-              services
-            </a>
-            <ul className="sub-ul">
-              <li>
-                <a href="">tec</a>
-              </li>
-              <li>
-                <a href="">online</a>
-              </li>
-              <li className="mini-projects">
-                <a href="">others&nbsp; + </a>
-                <ul className="sub-sub-ul">
-                  <li>
-                    <a href="">project 1</a>
-                  </li>
-                  <li>
-                    <a href="">project 2</a>
-                  </li>
-                  <li>
-                    <a href="">project 3</a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-          <li className="main-list">
-            <a className="main-link" href="#">
-              price{" "}
-            </a>
-            <ul className="sub-ul sub-of-js">
-              <li>
-                <a href="">coming soon🔥</a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </header>
-      <header className="show-when-mobile">
-        <h1>Courses 4 Arab</h1>
-        <label className="absolute" htmlFor="burger">
-          <i className="fas fa-bars" />
-        </label>
-        <input id="burger" type="checkbox" />
-        <div className="show-on-click">
-          <div className="main-div">
-            <label htmlFor="html">
-              HTML <i className="fas fa-plus" />
-            </label>
-            <input id="html" type="checkbox" />
-            <ul className="sub-div">
-              <li>
-                <a href="">Full Course</a>
-              </li>
-              <li>
-                <a href="">Crash Course</a>
-              </li>
-              <li>
-                <a href="">learn in 1h</a>
-              </li>
-            </ul>
-          </div>
-          <div className="main-div">
-            <label htmlFor="css">
-              CSS <i className="fas fa-plus" />
-            </label>
-            <input id="css" type="checkbox" />
-            <ul className="sub-div">
-              <li>
-                <a href="">Full Course</a>
-              </li>
-              <li>
-                <a href="">CSS Examples</a>
-              </li>
-              <li>
-                <label className="mini-projects" htmlFor="mini">
-                  mini projects <i className="fas fa-plus" />
-                </label>
-                <input id="mini" type="checkbox" />
-                <ul className="sub-sub-div">
-                  <li>
-                    <a href="">project 1</a>
-                  </li>
-                  <li>
-                    <a href="">project 2</a>
-                  </li>
-                  <li>
-                    <a href="">project 3</a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-          <div className="main-div">
-            <label htmlFor="js">
-              JavaScript <i className="fas fa-plus" />
-            </label>
-            <input id="js" type="checkbox" />
-            <ul className="sub-div">
-              <li>
-                <a href="">coming soon🔥</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </header>
+    <Header/>
       <main>Service</main>
       <div className="buttons">
         <button className="button">
@@ -142,10 +24,7 @@ const Service = () => {
         </button>
       </div>
 
-      <footer>
-        Designed by pioneer
-        <span>🧡</span>
-      </footer>
+    <Footer/>
     </>
   );
 };
