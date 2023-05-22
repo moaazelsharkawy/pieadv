589c0267c12bd357c3731e74ad65ea26a9b67d69
Moaaz <moaaz.elsharkawy2017@gmail.com>
2023-05-22 14:40:07 +0300

    formating

--- src/index.js
+++ src/index.js
@@ -1,70 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Home from './pages/home';
-import Erorr404 from './pages/404';
-import Online from './pages/online';
-import Price from './pages/price';
-import Service from './pages/service';
-import Company from './pages/company';
-import Product from './pages/product';
-import Tec from './pages/tec';
-
-
-
-
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home";
+import Erorr404 from "./pages/404";
+import Online from "./pages/online";
+import Price from "./pages/price";
+import Service from "./pages/service";
+import Company from "./pages/company";
+import Product from "./pages/product";
+import Tec from "./pages/tec";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home/>,
-    errorElement: <Erorr404/>,
+    element: <Home />,
+    errorElement: <Erorr404 />,
   },
 
   {
     path: "/online",
-    element: <Online/>,
-    errorElement: <Erorr404/>,
+    element: <Online />,
+    errorElement: <Erorr404 />,
   },
   {
     path: "/price",
-    element: <Price/>,
-    errorElement: <Erorr404/>,
+    element: <Price />,
+    errorElement: <Erorr404 />,
   },
   {
     path: "/service",
-    element: <Service/>,
-    errorElement: <Erorr404/>,
+    element: <Service />,
+    errorElement: <Erorr404 />,
   },
   {
     path: "/company",
-    element: <Company/>,
-    errorElement: <Erorr404/>,
+    element: <Company />,
+    errorElement: <Erorr404 />,
   },
   {
     path: "/product",
-    element: <Product/>,
-    errorElement: <Erorr404/>,
+    element: <Product />,
+    errorElement: <Erorr404 />,
   },
   {
     path: "/tec",
-    element: <Tec/>,
-    errorElement: <Erorr404/>,
+    element: <Tec />,
+    errorElement: <Erorr404 />,
   },
-
 ]);
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
