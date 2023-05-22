52cc68b4708fc103bfea2bcc65a65839894ddd36
Moaaz <moaaz.elsharkawy2017@gmail.com>
2023-05-22 14:34:42 +0300

    new pages

--- src/index.js
+++ src/index.js
@@ -2,11 +2,59 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/home';
+import Erorr404 from './pages/404';
+import Online from './pages/online';
+import Price from './pages/price';
+import Service from './pages/service';
+import Company from './pages/company';
+import Product from './pages/product';
 
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement: <Erorr404/>,
+  },
+
+  {
+    path: "/online",
+    element: <Online/>,
+    errorElement: <Erorr404/>,
+  },
+  {
+    path: "/price",
+    element: <Price/>,
+    errorElement: <Erorr404/>,
+  },
+  {
+    path: "/service",
+    element: <Service/>,
+    errorElement: <Erorr404/>,
+  },
+  {
+    path: "/company",
+    element: <Company/>,
+    errorElement: <Erorr404/>,
+  },
+  {
+    path: "/product",
+    element: <Product/>,
+    errorElement: <Erorr404/>,
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
